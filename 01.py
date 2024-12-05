ef4bab13bde79f328f6203aeb817965bd4472669
Aare-Kobar <aareaigar@hkhk.edu.ee>
2024-12-05 14:37:47 +0200

    kilpkonn kolmnurk

---
+++ 01.py
@@ -0,0 +1,19 @@
+# 01. ülesanne
+# Aare-Aigar 05.12.24
+
+
+# See impordib kilpkonna mooduli
+import turtle
+
+turtle.speed(1) # reguleeri 1-9
+turtle.penup()
+turtle.goto(-500,200)
+turtle.pendown()
+turtle.forward(200) #fd, pikslistes
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+
+# lõpetab kilpkonna, et ei jooksesks kokku
+turtle.done()
