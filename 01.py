71456707fcf31e25174ddb5635716723a2c42c28
Aare-Kobar <aareaigar@hkhk.edu.ee>
2024-12-05 14:53:05 +0200

    Update 01.py

--- 01.py
+++ 01.py
@@ -5,7 +5,8 @@
 # See impordib kilpkonna mooduli
 import turtle
 
-turtle.speed(1) # reguleeri 1-9
+# kolmnurk
+turtle.speed(5) # reguleeri 1-9
 turtle.penup()
 turtle.goto(-500,200)
 turtle.pendown()
@@ -14,6 +15,22 @@ turtle.left(120)
 turtle.forward(200)
 turtle.left(120)
 turtle.forward(200)
+
+# süda
+turtle.penup()
+turtle.goto(-200,200)
+turtle.pendown()
+turtle.left(120)
+turtle.fd(100)
+turtle.circle(50,180)
+turtle.right(90)
+turtle.circle(50,180)
+turtle.fd(100)
+
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+
 
 # lõpetab kilpkonna, et ei jooksesks kokku
 turtle.done()
