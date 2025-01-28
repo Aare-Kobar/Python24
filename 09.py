765d6a2b957d3631b3374478df38c64367b4c989
Aare <aareaigarkobar@gmail.com>
2025-01-28 16:34:57 +0200

    kujund

--- 09.py
+++ 09.py
@@ -12,7 +12,7 @@
 
 
 # Kasuta loendit 60, 5, 4, 42, 99, 67, 47, 22, 34, 8, 85, 50, 94, 39, 54, 83, 27, 40, 17, 75
-numbrid = [60, 5, 4, 42, 99, 67, 47, 22, 34, 8, 85, 50, 94, 39, 54, 83, 27, 40, 17, 75]
+# numbrid = [60, 5, 4, 42, 99, 67, 47, 22, 34, 8, 85, 50, 94, 39, 54, 83, 27, 40, 17, 75]
 
 
 # Leia paaris ja paaritud arvud ning lisa oma loendisse
@@ -58,7 +58,7 @@ numbrid = [60, 5, 4, 42, 99, 67, 47, 22, 34, 8, 85, 50, 94, 39, 54, 83, 27, 40, 
 # Leia keskmine läbisõidu ulatus ja hind
 # Kuva auto nimed, mille läbisõidu ulatus on suurem kui 300 km
 # Analüüsi andmeid, et tuvastada, kas kõrgema hinnaga autodel on tõepoolest pikem läbisõidu ulatus
-ev_data = [
+""" ev_data = [
 ['vehicle', 'range', 'price'],
 ['Tesla Model Y Long Range', '330', '58990'],
 ['Volkswagen ID.4 Pro', '260', '39995'],
@@ -81,7 +81,42 @@ print(f"Keskmine ulatus: {sum(ranges)/len(ranges)} km")
 
 for autod in ev_data:
     if int(autod[1]) > 300:
-        print(autod[0])
+        print(autod[0]) """
 
     # for i in autod:
     #     print(i)
+    
+# 14.  Kasuta Python Turtle moodulit, et tsüklite abil luua järgmised kujundid.
+#  * Kasuta muutujaid ja nendevahelisi seoseid, et kujundid oleks skaleeritavad 
+
+import turtle
+
+kylg = 100
+
+sc = turtle.Screen()
+sc.setup(startx = 600, starty = 0) 
+
+turtle.pensize(2)
+turtle.shape("turtle")
+turtle.speed(5)
+
+for i in range(6):
+    for y in range(1):
+        turtle.forward(kylg)
+        turtle.left(120)
+        turtle.forward(kylg)
+        turtle.left(120)
+        turtle.forward(kylg)
+        turtle.left(120)
+        turtle.forward(kylg)
+        turtle.left(60)
+        turtle.forward(kylg)
+        turtle.left(120)
+        turtle.forward(kylg)
+        turtle.left(60)
+        turtle.forward(kylg)
+        turtle.left(120)
+    turtle.right(60)
+    
+turtle.hideturtle()
+turtle.done()
