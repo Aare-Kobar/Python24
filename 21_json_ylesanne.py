4910b16980870ebb0ae4af7b1b9ad814c5e1c823
Aare <aareaigar@hkhk.edu.ee>
2025-03-13 16:57:17 +0200

    aa

--- 21_json_ylesanne.py
+++ 21_json_ylesanne.py
@@ -4,12 +4,13 @@
 # Lahenda vähemalt 3 ülesandes kirjeldatud nõuet. Kirjuta selge ja loetav kood, kasuta asjakohaseid kommentaare ning muutujaid nimeta arusaadavalt.
 # Esita oma töö GitHubi lingina.
 
-# 4. raamatud.json
-# •	Leia, mitu raamatut ilmus enne 2000. aastat.
-# •	Leia kõik raamatud, mis pole saadaval.
-# •	Leia vanim raamat andmestikus.
-# •	Leia žanr, mida esineb andmetes kõige sagedamini.
-# •	Leia, mitu raamatut on välja antud pärast aastat 2010.
+# 3. uritused.json
+# •	Leia, mitu üritust toimub Tallinnas.
+# •	Leia kõik üritused, mis toimuvad pärast kellaaega 18:00.
+# •	Leia kuu, kus toimub kõige rohkem üritusi.
+# •	Leia kõige varasem ja kõige hilisem üritus kalendris.
+# •	Loetle kõik üritused, mis toimuvad nädalapäeval "laupäev" või "pühapäev".
+# ________________________________________
 
 import requests
 
@@ -19,14 +20,15 @@ response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    uritused = data.get("uritused")
 
+    uritused = data["uritused"]
 
+    urituste_arv = data.count("Tallinn")
 
-    for i in uritused:
-        if i
-        else:
+    for uritus in uritused:
+        if (urituste_arv["koht"]) == "Tallinn":
+            print(f"{urituste_arv} üritust toimub Tallinnas.")
 
 
 else: 
-    print(f"Viga! Ei suutnud andmeid laadida. Staatuskood: {response.status_code}")
+    print(f"Päring ebaõnnestus. Staatuskood: {response.status_code}")
