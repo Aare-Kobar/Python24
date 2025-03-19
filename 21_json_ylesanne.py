724d6693f3ce5d4ffa01776a1768d19092813322
Aare <aareaigarkobar@gmail.com>
2025-03-19 13:43:52 +0200

    19.03

--- 21_json_ylesanne.py
+++ 21_json_ylesanne.py
@@ -13,22 +13,55 @@
 # ________________________________________
 
 import requests
+from datetime import datetime
+from collections import Counter
+import calendar
+import locale 
+locale.setlocale(locale.LC_TIME, 'et_EE.UTF-8') 
 
 url = "https://www.metshein.com/kordamine/json/uritused.json"
-response = requests.get(url)
+response = requests.get(url) # Funktsioon saadab HTTP GET päringu määratud URL-ile. Serveri vastus salvestatakse muutujasse.
 
+if response.status_code == 200: # Kontrollib, kas HTTP vastuse olekukood on 200, mis tähendab, et päring oli edukas.
+    data = response.json() # Võtab serverist saadud vastuse ja teisendab selle JSON-formaadist Python'i andmeteks
 
-if response.status_code == 200:
-    data = response.json()
+    uritused = data["uritused"] # Võtab sündmuste andmed JSON failist, kasutadaes võtmesõna "uritused"
 
-    uritused = data["uritused"]
+    # Leian mitu üritust toimub Tallinnas
+    tallinn_uritus = 0 # Ürituste arv mis toimub Tallinnas
 
-    urituste_arv = data.count("Tallinn")
+    for uritus in uritused: # Tsükkel käib läbi kõik üritused
+        if uritus["koht"] == "Tallinn":
+            tallinn_uritus +=1 # Kui üritus toimub Tallinnas, siis suurendab tallinn_uritus loendurit ühe võrra
+
+    print(f"{tallinn_uritus} üritust toimub Tallinnas")
+
+    #Leian kõik üritused mis toimuvad pärast kellaaega 18:00.
+    ohtused_uritused = [] # Loob tühja nimekirja hiliste ürituste jaoks
 
     for uritus in uritused:
-        if (urituste_arv["koht"]) == "Tallinn":
-            print(f"{urituste_arv} üritust toimub Tallinnas.")
+        urituse_aeg = datetime.strptime(uritus["kellaaeg"], "%H:%M").time() # Teisendab kallaaja stringi datetime objektiks
+        ohtu_aeg = datetime.strptime("18:00", "%H:%M").time() # Võtab kellaaja 18:00 võrdluseks
+        
+        if urituse_aeg > ohtu_aeg: # Kontrollib, kas üritus toimub pärast 18:00
+            ohtused_uritused.append(uritus) # Lisab sobiva ürituse nimekirja
+
+    print("Üritused pärast kellaaega 18:00:") # Kuvab kõik leitud õhtused üritused
+    for uritus in ohtused_uritused:
+        print(uritus)
 
 
+    # Leian kuu, kus toimub kõige rohkem üritusi
+    kuu_loendur = Counter() # Loendur kuude arvestamiseks
+
+    for uritus in uritused:
+        kuupaev = datetime.strptime(uritus["kuupäev"], "%Y-%m-%d") # Muudab ürituse kuupäeva datetime objektiks
+        kuu_loendur[kuupaev.month] += 1 # Loendab üritusi iga kuu kohta
+    
+    koige_rohkem_uritusi_kuu, urituste_arv = kuu_loendur.most_common(1)[0] # Leiab kõige sagedamini esineva kuu ja ürituste arvu
+    kuu_nimi = calendar.month_name[koige_rohkem_uritusi_kuu] # Teisendab kuu numbri nimeks
+
+    print(f"Kõige rohkem üritusi toimub kuus {kuu_nimi} ({urituste_arv} üritust).")
+    
 else: 
     print(f"Päring ebaõnnestus. Staatuskood: {response.status_code}")
